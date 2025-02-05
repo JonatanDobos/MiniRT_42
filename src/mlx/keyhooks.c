8350ae25a6d2230405d18658797594d4d364bbdb
Remy De Bruijn <rde-brui@f0r6s14.codam.nl>
2025-02-05 18:02:40 +0100

    mouse selection works

--- src/mlx/keyhooks.c
+++ src/mlx/keyhooks.c
@@ -6,7 +6,7 @@
 /*   By: rde-brui <rde-brui@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/02/05 15:12:14 by rde-brui      #+#    #+#                 */
-/*   Updated: 2025/02/05 16:54:14 by rde-brui      ########   odam.nl         */
+/*   Updated: 2025/02/05 18:01:19 by rde-brui      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,9 @@
 #include <RTmlx.h>
 #include <scene.h>
 #include <debug.h>
+#include <render.h>
 
+#include <mathRT.h>
 #include <stdio.h>
 # include <MLX42/MLX42.h>
 
@@ -39,18 +41,53 @@
 // 	return (pixel_color);
 // }
 
+t_vec4 mouse_clicks_on_obj(t_scene *scene, t_ray ray)
+{
+	float		closest_t;
+	uint8_t		closest_intersect_type;
+	t_vec4		pixel_color;
+	t_vec4		normal;
+	uint32_t	closest_obj;
+	t_vec4		hit_point;
+
+	pixel_color = (t_vec4){0.0F, 0.0F, 0.0F, 1.0F};
+	closest_obj = find_closest_object(scene, ray, &closest_t, &closest_intersect_type);
+	if (closest_obj != 0)
+	{
+		if (scene->selected_obj == NULL || closest_obj != scene->sel_obj_index)
+		{
+			scene->sel_obj_index = closest_obj;
+			scene->selected_obj = scene->objs + closest_obj;
+		}
+		else
+		{
+			scene->selected_obj = NULL;
+		}
+	}
+}
 
 void	mouse_hook(mouse_key_t button, action_t action, modifier_key_t mods, t_rt *rt)
 {
-	if (button == MLX_MOUSE_BUTTON_LEFT)
-		printf("check\n");
-	
-	int32_t	x;
-	int32_t	y;
-	
-	mlx_get_mouse_pos(rt->win->mlx, &x, &y);
+	static bool iets = false;
+	t_axis2 pos;
+	float	ndc_x;
+	float	ndc_y;
+	t_ray	ray;
 
-	printf("x = %d\ty = %d\n", x, y);
+	if (iets == false)
+	{
+		mlx_get_mouse_pos(rt->win->mlx, &pos.x, &pos.y);
+		ndc_x = (2.0F * ((pos.x + 0.5F) / (float)rt->win->rndr_wdth) - 1.0F) * rt->win->aspectrat;
+		ndc_y = 1.0F - 2.0F * ((pos.y + 0.5F) / (float)rt->win->rndr_hght);
+		ray.origin = rt->scene->camera.coords;
+		ray.vec = transform_ray_dir((t_vec4){ndc_x, ndc_y, rt->scene->camera.c.zvp_dist, 0.0F}, rt->scene->camera.c.orientation);
+		mouse_clicks_on_obj(rt->scene, ray);
+		iets = true;
+	}
+	else
+	{
+		iets = false;
+	}
 }
 
 void	my_keyhook(mlx_key_data_t keydata, t_rt *rt)
@@ -74,7 +111,6 @@ void	my_keyhook(mlx_key_data_t keydata, t_rt *rt)
 
 void	init_hooks(t_rt *rt)
 {
-
 	mlx_key_hook(rt->win->mlx, (mlx_keyfunc)my_keyhook, rt);
 	mlx_loop_hook(rt->win->mlx, (mlx_closefunc)loop_hook, rt);
 	mlx_mouse_hook(rt->win->mlx, (mlx_mousefunc)mouse_hook, rt);
