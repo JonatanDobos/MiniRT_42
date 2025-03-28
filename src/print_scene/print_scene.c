b4a123dd81a58a7403a3f872c8a6d4640b0bfafd
Remy De Bruijn <rde-brui@f1r2s3.codam.nl>
2025-03-28 19:26:11 +0100

    Printing the whole scene to .rt, only split into files, and make keybinding

--- src/print_scene/print_scene.c
+++ src/print_scene/print_scene.c
@@ -62,7 +62,7 @@ size_t	coords_line(t_dbltoa *dbl, char *rt_line, t_vec4 coords)
 	return (line_index);
 }
 
-void	ambient_line(t_dbltoa *dbl, t_objs *ambient)
+void	ambient_line(t_objs *ambient, t_dbltoa *dbl, int fd)
 {
 	char	rt_line[RT_MAX_LINE_LEN];
 	size_t	line_index;
@@ -73,10 +73,10 @@ void	ambient_line(t_dbltoa *dbl, t_objs *ambient)
 	line_index += cpy_str(rt_line + line_index, dbl->buff);
 	line_index += cpy_str(rt_line + line_index, "\t\t");
 	line_index += color_line(dbl, rt_line + line_index, ambient->color);
-	printf("%s\n", rt_line);
+	ft_putendl_fd(rt_line, fd);
 }
 
-void	camera_line(t_dbltoa *dbl, t_objs *camera)
+void	camera_line(t_objs *camera, t_dbltoa *dbl, int fd)
 {
 	char	rt_line[RT_MAX_LINE_LEN];
 	size_t	line_index;
@@ -88,10 +88,10 @@ void	camera_line(t_dbltoa *dbl, t_objs *camera)
 	dbl->precision = 0;
 	dbltoa_buff_prec(*dbl);
 	cpy_str(rt_line + line_index, dbl->buff);
-	printf("%s\n", rt_line);
+	ft_putendl_fd(rt_line, fd);
 }
 
-void	lights_line(t_dbltoa *dbl, t_objs *lights, size_t amount)
+void	lights_line(t_objs *lights, size_t amount, t_dbltoa *dbl, int fd)
 {
 	char	rt_line[RT_MAX_LINE_LEN];
 	size_t	line_index;
@@ -112,32 +112,103 @@ void	lights_line(t_dbltoa *dbl, t_objs *lights, size_t amount)
 
 
 		line_index += color_line(dbl, rt_line + line_index, lights[i].color);
-		printf("%s\n", rt_line);
+		ft_putendl_fd(rt_line, fd);
 		++i;
 	}
 }
+
+void	plane_line(t_objs *pl, t_dbltoa *dbl, int fd)
+{
+	char	rt_line[RT_MAX_LINE_LEN];
+	size_t	line_index;
+
+	line_index = cpy_str(rt_line, "pl\t");
+	line_index += coords_line(dbl, rt_line + line_index, pl->coords);
+	line_index += coords_line(dbl, rt_line + line_index, pl->plane.orientation);
+	line_index += color_line(dbl, rt_line + line_index, pl->color);
+	ft_putendl_fd(rt_line, fd);
+}
+
+void	sphere_line(t_objs *sp, t_dbltoa *dbl, int fd)
+{
+	char	rt_line[RT_MAX_LINE_LEN];
+	size_t	line_index;
+
+	line_index = cpy_str(rt_line, "sp\t");
+	line_index += coords_line(dbl, rt_line + line_index, sp->coords);
+
+	dbl->value = sp->sphere.diameter;
+	dbltoa_buff_prec(*dbl);
+	line_index += cpy_str(rt_line + line_index, dbl->buff);
+	line_index += cpy_str(rt_line + line_index, "\t\t");
+	line_index += color_line(dbl, rt_line + line_index, sp->color);
+	ft_putendl_fd(rt_line, fd);
+}
+
+void	cylinder_line(t_objs *cy, t_dbltoa *dbl, int fd)
+{
+	char	rt_line[RT_MAX_LINE_LEN];
+	size_t	line_index;
+
+	line_index = cpy_str(rt_line, "cy\t");
+	line_index += coords_line(dbl, rt_line + line_index, cy->coords);
+	line_index += coords_line(dbl, rt_line + line_index, cy->cylinder.orientation);
+	dbl->value = cy->cylinder.diameter;
+	dbltoa_buff_prec(*dbl);
+	line_index += cpy_str(rt_line + line_index, dbl->buff);
+	line_index += cpy_str(rt_line + line_index, "\t\t");
+
+	dbl->value = cy->cylinder.height;
+	dbltoa_buff_prec(*dbl);
+	line_index += cpy_str(rt_line + line_index, dbl->buff);
+	line_index += cpy_str(rt_line + line_index, "\t\t");
+
+	line_index += color_line(dbl, rt_line + line_index, cy->color);
+	ft_putendl_fd(rt_line, fd);
+}
+
+void	objs_line(t_objs *objs, size_t amount, t_dbltoa *dbl, int fd)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < amount)
+	{
+		if (objs[i].type == PLANE)
+			plane_line(objs + i, dbl, fd);
+		else if (objs[i].type == SPHERE)
+			sphere_line(objs + i, dbl, fd);
+		else if (objs[i].type == CYLINDER)
+			cylinder_line(objs + i, dbl, fd);
+		++i;
+	}
+
+}
+
 void	create_scene_rt_file(t_scene *sc)
 {
-	// char	filename[FILE_CREATION];
-	// int		fd;
+	char	filename[FILE_CREATION];
+	int		fd;
 
-	// if (get_user_input(filename, FILE_CREATION, NAME_FILE) <= 0)
-	// 	return ;
-	// fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0644); 
-	// if (fd == -1) {
-	// 	perror("create_scene_rt_file: Error opening file");
-	// 	return ;
-	// }
+	if (get_user_input(filename, FILE_CREATION, NAME_FILE) <= 0)
+		return ;
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0644); 
+	if (fd == -1) {
+		perror("create_scene_rt_file: Error opening file");
+		return ;
+	}
 
-	char	dbltoa_buff[MAX_DBL_BUFF];
+	char		dbltoa_buff[MAX_DBL_BUFF];
 	t_dbltoa	dbl;
 
 	dbl = setup_dbltoa_params(dbltoa_buff);
 
-	ambient_line(&dbl, &sc->ambient);
-	camera_line(&dbl, &sc->camera);
-	lights_line(&dbl, sc->lights, sc->l_arr_size);
+	ambient_line(&sc->ambient, &dbl, fd);
+	camera_line(&sc->camera, &dbl, fd);
+	lights_line(sc->lights, sc->l_arr_size, &dbl, fd);
+	objs_line(sc->objs, sc->o_arr_size, &dbl, fd);
 
+	close(fd);
 // printf("camera: %f  %f  %f\n", sc->camera.coords[0], sc->camera.coords[1], sc->camera.coords[2]);
 	puts("worked");
 	exit(0);
