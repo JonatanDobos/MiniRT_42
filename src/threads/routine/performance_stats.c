8aa061b4ea5a97fd27a18a583bac5bd2efc7ff4d
Remy De Bruijn <rde-brui@f1r2s3.codam.nl>
2025-05-02 13:16:23 +0200

    performance_stats.c formatted in our general overlay style

--- src/threads/routine/performance_stats.c
+++ src/threads/routine/performance_stats.c
@@ -7,17 +7,57 @@
 #include <limits.h>
 #include <dbltoa.h>
 
-int	powi(int base, int pow)
-{
-	const int	start_base = base;
+//	Static Functions
+static int	powi(int base, int pow);
+static char	*itoa_simple(int num, char *buffer);
+static char	*ftoa_simple(float num, char *buffer, uint8_t prec);
+static void	fill_print_buffer(t_rt *rt, float avg_fps, char *buffer);
 
-	while (pow-- > 1)
-		base *= start_base;
-	return (base);
+void	print_performance_stats(t_rt *rt, float fps)
+{
+	static float	fps_average = 0.0F;
+	static uint8_t	counter = 0;
+	static double	last_print_time = 0.0;
+	const double	cur_time = mlx_get_time();
+	char			buffer[PRT_BUFFER_SIZE];
+
+	fps_average += fps;
+	++counter;
+	if (rt->win->res_ratio == rt->win->res_r_start \
+		&& cur_time < last_print_time + 0.3)
+		return ;
+	last_print_time = cur_time;
+	if (rt->win->prt_perf_stats == true)
+	{
+		fill_print_buffer(rt, fps_average / (float)counter, buffer);
+		pthread_mutex_lock(rt->mtx + MTX_PRINT);
+		ft_putstr_fd("\r\33[2K\033[94m", STDOUT_FILENO);
+		if (rt->win->res_ratio == RES_R_FULL)
+			ft_putstr_fd("\033[92m", STDOUT_FILENO);
+		ft_putstr_fd(buffer, STDOUT_FILENO);
+		ft_putstr_fd("\033[0m", STDOUT_FILENO);
+		pthread_mutex_unlock(rt->mtx + MTX_PRINT);
+	}
+	counter = 0;
+	fps_average = 0;
+}
+
+static void	fill_print_buffer(t_rt *rt, float avg_fps, char *buffer)
+{
+	if ((int)avg_fps > INT_MAX || avg_fps < 0.0F)
+		return ;
+	ft_bzero(buffer, PRT_BUFFER_SIZE);
+	ft_strlcpy(buffer, " ResStep ", 10);
+	buffer += 9;
+	buffer = itoa_simple(rt->win->res_ratio, buffer);
+	ft_strlcpy(buffer, " -> ", 5);
+	buffer += 4;
+	buffer = ftoa_simple(avg_fps, buffer, 1);
+	ft_strlcpy(buffer, " fps(avg)", 10);
 }
 
 // Simple itoa, If buffer is too small: overflow (Segfault).
-char	*itoa_simple(int num, char *buffer)
+static char	*itoa_simple(int num, char *buffer)
 {
 	size_t		len;
 	size_t		i;
@@ -46,9 +86,18 @@ char	*itoa_simple(int num, char *buffer)
 	return (buffer + len);
 }
 
+static int	powi(int base, int pow)
+{
+	const int	start_base = base;
+
+	while (pow-- > 1)
+		base *= start_base;
+	return (base);
+}
+
 // Simple ftoa, If buffer size is too small: overflow (Segfault).
 // Min precicion = 1, max = 255.
-char	*ftoa_simple(float num, char *buffer, uint8_t prec)
+static char	*ftoa_simple(float num, char *buffer, uint8_t prec)
 {
 	int		int_part;
 	int		digit;
@@ -70,48 +119,4 @@ char	*ftoa_simple(float num, char *buffer, uint8_t prec)
 		frac -= digit;
 	}
 	return (buffer[i] = '\0', buffer + i);
-}
-
-static void	fill_print_buffer(t_rt *rt, float avg_fps, char *buffer)
-{
-	if ((int)avg_fps > INT_MAX || avg_fps < 0.0F)
-		return ;
-	ft_bzero(buffer, PRT_BUFFER_SIZE);
-	ft_strlcpy(buffer, " ResStep ", 10);
-	buffer += 9;
-	buffer = itoa_simple(rt->win->res_ratio, buffer);
-	ft_strlcpy(buffer, " -> ", 5);
-	buffer += 4;
-	buffer = ftoa_simple(avg_fps, buffer, 1);
-	ft_strlcpy(buffer, " fps(avg)", 10);
-}
-
-void	print_performance_stats(t_rt *rt, float fps)
-{
-	static float	fps_average = 0.0F;
-	static uint8_t	counter = 0;
-	static double	last_print_time = 0.0;
-	const double	cur_time = mlx_get_time();
-	char			buffer[PRT_BUFFER_SIZE];
-
-	fps_average += fps;
-	++counter;
-	if (rt->win->res_ratio == rt->win->res_r_start \
-		&& cur_time < last_print_time + 0.3)
-		return ;
-	last_print_time = cur_time;
-	ft_putstr_fd("\r\33[2K", STDOUT_FILENO);
-	if (rt->win->prt_perf_stats == true)
-	{
-		fill_print_buffer(rt, fps_average / (float)counter, buffer);
-		pthread_mutex_lock(rt->mtx + MTX_PRINT);
-		ft_putstr_fd("\033[94m", STDOUT_FILENO);
-		if (rt->win->res_ratio == RES_R_FULL)
-			ft_putstr_fd("\033[92m", STDOUT_FILENO);
-		ft_putstr_fd(buffer, STDOUT_FILENO);
-		ft_putstr_fd("\033[0m", STDOUT_FILENO);
-		pthread_mutex_unlock(rt->mtx + MTX_PRINT);
-	}
-	counter = 0;
-	fps_average = 0;
 }
